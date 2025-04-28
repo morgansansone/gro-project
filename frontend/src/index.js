ca254599d9a048a2dd4508cb12485377413d108b
IsaOrtiz08 <ortizisa1008@gmail.com>
2025-04-28 12:35:55 +0530

    stopped duplicates of browser in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
