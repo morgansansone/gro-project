ab59ff71827f5953f3eb93069ad57301fcfbb6ee
morgansansone <morgansansone00@gmail.com>
2025-05-01 23:42:02 -0500

    Added goal sheet created by ME

--- frontend/src/pages/dash/LearnMore.js
+++ frontend/src/pages/dash/LearnMore.js
@@ -217,7 +217,7 @@ export default function LearnMore() {
                 </a>
 
                 <a
-                  href="/goal-planning-worksheet.pdf"
+                  href="/goal_planning_worksheet.pdf"
                   download
                   className="mt-4 inline-block bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition"
                 >
